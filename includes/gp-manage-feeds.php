6e35f1f725d8dd7700d2501102d0a8604a29e40a
jduquette <jduquette@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-27 20:20:53 +0000

    Tagged 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/grabpress/tags/2.0.3@645114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/gp-manage-feeds.php
+++ includes/gp-manage-feeds.php
@@ -11,8 +11,12 @@
 	<table class="grabpress-table manage-table" cellspacing="0">
 		<tr>
 			<th>Active</th>
-			<th>Video<br/>Categories</th>
+			<th>Name</th>
+			<th>Video Categories</th>
 			<th>Keywords</th>
+			<th>Exclude<br/>Keywords</th>
+			<th>Exact<br/>Phrase</th>
+			<th>Any<br/>keyword</th>
 			<th>Content<br/>Providers</th>			
 			<th>Schedule</th>
 			<th>Max<br/>Results</th>
@@ -27,19 +31,21 @@
 		<?php
 			$feeds = GrabPress::get_feeds();
 			$num_feeds = count( $feeds );
-			$json = GrabPress::get_json( 'http://catalog.'.GrabPress::$environment.'.com/catalogs/1/categories' );
-			$categories_list = json_decode( $json );
+			
 
 			for ( $n = 0; $n < $num_feeds; $n++ ) {
 				$feed = $feeds[$n]->feed;
 				$url = array();
 				parse_str( parse_url( $feed->url, PHP_URL_QUERY ), $url );
+				GrabPress::_escape_params_template($url);
 				$feedId = $feed->id;
 				$providers = explode( ",", $url["providers"] ); // providers chosen by the user
+				$channels = explode( ",", $url["categories"] ); // Video categories chosen by the user
 		?>
 		<form id="form-<?php echo $feedId; ?>" action=""  method="post">
 			<input type="hidden" id="action-<?php echo $feedId; ?>" name="action" value="" />
 			<input type="hidden" name="referer" value="edit" />
+			<input type="hidden" name="channels_total" value="<?php echo $channels_total; ?>" id="channels_total" />	
 			<?php 
 				if(isset($_GET['action']) && ($_GET['action']=='edit-feed') && ($_GET['feed_id']==$feedId)){
 					$row_class = "editing-feed";
@@ -57,26 +63,62 @@
 							echo $checked = ( $feed->active  ) ? 'Yes' : 'No'; 
 					 	}else{ 
 							$checked = ( $feed->active  ) ? 'checked = "checked"' : '';
-							echo '<input '.$checked.' type="checkbox" onclick="toggleButton('.$feedId.')" value="1" name="active" class="active-check" id="active-check-'.$feedId.'" />';
+							echo '<input '.$checked.' type="checkbox" value="1" name="active" class="active-check" id="active-check-'.$feedId.'" />';
 						} 
-					?>
-				</td>
-				<td>							
-					<?php
-						foreach ( $categories_list as $record ) {
-							$category = $record -> category;
-							$name = $category -> name;
-							$id = $category -> id;
-							if($name == $feed->name){
-								echo $name;
-							}									
-						}
 					?>
 				</td>
 				<td>		
 					<?php 
-						$keywords_and_num = strlen($url['keywords_and']);
-						echo $keywords_and = ($keywords_and_num > 15) ? substr($url['keywords_and'],0,15)."..." : $url['keywords_and'];
+						echo urldecode($feed->name);
+					?>							
+				</td>
+				<td>		
+					<?php 
+						$video_categories_array = explode(",", $url['categories']);
+						$video_categories_num = count($video_categories_array);
+						if($url['categories'] == ""){
+							echo "All Video Categories";
+						}else if($video_categories_num == 1){							
+							echo $video_categories = ($video_categories_num > 15) ? substr($url['categories'],0,15)."..." : $url['categories'];
+						}else{
+							echo $video_categories_num." selected";
+						}
+					?>							
+				</td>
+				<td>		
+					<?php 
+						if(isset($url['keywords_and'])){
+							$keywords_and_num = strlen($url['keywords_and']);
+							$keywords_and = $url['keywords_and'];
+							echo $keywords_and = ($keywords_and_num > 15) ? substr($keywords_and,0,15)."..." : $keywords_and;
+						}
+					?>							
+				</td>
+				<td>		
+					<?php 
+						if(isset($url['keywords_not'])){
+							$keywords_not_num = strlen($url['keywords_not']);
+							$keywords_not = $url['keywords_not'];
+							echo ($keywords_not_num > 15) ? substr($keywords_not,0,15)."..." : $keywords_not;
+						}
+					?>							
+				</td>
+				<td>		
+					<?php 
+						if(isset($url['keywords_phrase'])){
+							$keywords_phrase_num = strlen($url['keywords_phrase']);
+							$keywords_phrase = $url['keywords_phrase'];
+							echo $keywords_phrase = ($keywords_phrase_num > 15) ? substr($keywords_phrase,0,15)."..." : $keywords_phrase;
+						}
+					?>							
+				</td>
+				<td>		
+					<?php 
+						if(isset($url['keywords'])){
+							$keywords_or_num = strlen($url['keywords']);
+							$keywords = $url['keywords'];
+							echo $keywords_or = ($keywords_or_num > 15) ? substr($keywords,0,15)."..." : $keywords;
+						}
 					?>							
 				</td>
 				<td>
@@ -87,7 +129,7 @@
 							if ( in_array( "", $providers ) ) {
 								echo "All providers";
 							}else{	
-								foreach ( $list_provider as $record_provider ) {
+								foreach ( $list_providers as $record_provider ) {
 									$provider = $record_provider->provider;
 									$provider_name = $provider->name;
 									$provider_id = $provider->id;											
@@ -97,7 +139,7 @@
 								}
 							}
 						}else{
-							echo $providers_selected." providers selected of ".$providers_total;
+							echo $providers_selected." selected";
 						}
 					?>
 				</td>
@@ -108,8 +150,8 @@
 							$values = array( 15,  30,  45, 60, 120, 360, 720, 1440, 2880, 4320 );
 						}
 						else {
-							$times = array( '12 hrs', '01 day', '02 days', '03 days' );
-							$values = array( 720, 1440, 2880, 4320 );
+							$times = array( '06 hrs', '12 hrs', '01 day', '02 days', '03 days' );
+							$values = array( 360, 720, 1440, 2880, 4320 );
 						}
 						for ( $o = 0; $o < count( $times ); $o++ ) {
 							$time = $times[$o];
@@ -183,13 +225,13 @@
 					}
 				?>
 				<td>
-					<a href="#" onclick="previewFeed(<?php echo $feedId; ?>);return false;" id="btn-preview-feed-<?php echo $feedId; ?>" class="<?php echo $class_preview_button; ?>" >preview</a>
+					<a href="admin.php?page=autoposter&action=preview-feed&feed_id=<?php echo $feedId; ?>"  id="btn-preview-feed-<?php echo $feedId; ?>" class="<?php echo $class_preview_button; ?> btn-preview-feed" >preview</a>
 				</td>
 				<td>
 					<?php if(isset($_GET['action']) && ($_GET['action']=='edit-feed') && ($_GET['feed_id']==$feedId)){ 
 						echo $text_edit_button;
 					 }else{ ?>				
-					<a href="#" onclick="editFeed(<?php echo $feedId; ?>);return false;" id="btn-update-<?php echo $feedId; ?>" class="<?php echo $class_edit_button; ?>">						
+					<a href="admin.php?page=autoposter&action=edit-feed&feed_id=<?php echo $feedId; ?>" id="btn-update-<?php echo $feedId; ?>" class="<?php echo $class_edit_button; ?> btn-update-feed">						
 						<?php echo $text_edit_button; ?>
 					</a>
 					<?php } ?>
